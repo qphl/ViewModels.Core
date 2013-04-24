83694729f94e46283ac8ac14fb39525506581cd4
Rich Knight <richard.knight@bathasu.com>
2013-04-24 16:29:57 +0100

    cleaned up references

--- src/tests/RavenDBViewModelRepositoryTests.cs
+++ src/tests/RavenDBViewModelRepositoryTests.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using CR.ViewModels.Core;
-using CR.ViewModels.Persistance.RavenDB;
+﻿using CR.ViewModels.Persistance.RavenDB;
 using NUnit.Framework;
 using Raven.Client;
 using Raven.Client.Document;
@@ -10,26 +8,15 @@ namespace CR.ViewModels.Tests
 {
     public class RavenDBViewModelRepositoryTests : ViewModelRepositoryTestFixture
     {
-        private static int DataDirectories = 0;
         private IDocumentStore _docStore;
 
         [SetUp]
         public void SetUp()
         {
-            //_docStore = new EmbeddableDocumentStore() {DataDirectory = "data" + ++DataDirectories};
             _docStore = new EmbeddableDocumentStore() {RunInMemory = true, Conventions = new DocumentConvention() {DefaultQueryingConsistency = ConsistencyOptions.QueryYourWrites}};
             _docStore.Initialize();
             Reader = new RavenDBViewModelReader(_docStore);
             Writer = new RavenDBViewModelWriter(_docStore);
         }
-
-        /*[TearDown]
-        public void TearDown()
-        {
-            _docStore.Dispose();
-            var dir = new DirectoryInfo("data");
-            dir.Delete(true);
-        }*/
-
     }
 }
