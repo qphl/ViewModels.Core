e26586bdf684971fb58e43e012a1d071814d7e89
Isaac <23095702+iBeizsley@users.noreply.github.com>
2018-06-20 09:49:56 +0100

    Remove 'tags/' from tag name in Nuget package release notes link.

--- build.sh
+++ build.sh
@@ -7,7 +7,8 @@ fi
 tag="0.0.0"
 if [ -n "$2" ]; then tag="$2"
 fi
+tag=${tag/tags\//}
 
-dotnet test src/ViewModels.Core.Tests/ViewModels.Core.Tests.csproj
+dotnet test .\\src\\ViewModels.Core.Tests\\ViewModels.Core.Tests.csproj
 
-dotnet pack src/ViewModels.Core/ViewModels.Core.csproj -o ../../dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
+dotnet pack .\\src\\ViewModels.Core\\ViewModels.Core.csproj -o ..\\..\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
